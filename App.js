8fcb66b56389f3f2ce6ebdbc3f6ecd03796a7469
nascott <nascott94@outlook.com>
2021-12-17 09:46:06 -0700

    gamescreen added to app.js as well as gamehandler function

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 import Header from "./components/Header";
+import GameScreen from "./screens/GameScreen";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
+  const [userNumber, setuserNumber] = useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setuserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen />;
+
+  if (userNumber) {
+    content = <GameScreen />;
+  }
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
